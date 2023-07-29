5cae8ba70175246e3d2214fd821d345c42e6f858
Malix-Off <alixbrunetcontact@gmail.com>
2023-07-30 00:40:21 +0200

    env + init error handling

--- src/main.go
+++ src/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"log"
 	"os"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	godotenv.Load("../.env")
+
+	client, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+        log.Fatal(err)
+    }
+
+    // Start the bot.
+    err = client.Open()
+    if err != nil {
+        log.Fatal(err)
+    }
+
 }
