42452e1cb09b2b29eb9d092e59323980b736ddf9
Malix-Off <alixbrunetcontact@gmail.com>
2023-08-03 16:54:00 +0200

    Feat: comments

--- src/pkg/errorHandling/errorHandling.go
+++ src/pkg/errorHandling/errorHandling.go
@@ -2,12 +2,14 @@ package errorHandling
 
 import "log"
 
+// if err, fatal
 func FatalCheck(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// if err, log
 func LogCheck(err error) {
 	if err != nil {
 		log.Print(err)
